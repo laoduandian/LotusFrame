697ffc348e85b07a46155870ed6976ee34a0c3a7
laoduandian <laoduandian@sina.com>
2019-08-17 12:42:21 +0800

    1

--- androidlibrary/src/main/java/lotus/net/center/android/ad/LotusAd.java
+++ androidlibrary/src/main/java/lotus/net/center/android/ad/LotusAd.java
@@ -3,13 +3,13 @@ package lotus.net.center.android.ad;
 import android.widget.RelativeLayout;
 
 public interface LotusAd {
-	public void initInterstitialAd();
-	public void loadInsertscreen();
-	public void showInsertscreen();
-	public void initRewardedVideo();
-	public void loadRewardedVideo();
-	public void initBannerAd(RelativeLayout bannerRelativeLayout);
-	public void dispose();
+	void initInterstitialAd();
+	void loadInsertscreen();
+	void showInsertscreen();
+	void initRewardedVideo();
+	void loadRewardedVideo();
+	void initBannerAd(RelativeLayout bannerRelativeLayout);
+	void dispose();
 	void addBanners(boolean isHead);
 	void removeRanners();
 	void showMovie(int id);
